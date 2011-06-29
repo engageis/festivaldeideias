94cf9fa9a0e4cc0fd5fbe5142911a02fe05114f8
Daniel Weinmann <danielweinmann@gmail.com>
2011-06-28 22:47:31 -0300

    Creating Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ramify::Application.config.secret_token = '888a01673396bb42e3b0b1a1ada4509180abe711c72a416afa9ce608de42606bd89c3dacf62ca5662458b91d9d29e2e558ce55a014165cb1eb0573d9b041ae90'
