78b0abb0f170e87c9e57ad122bd06fe15c693173
Daniel Weinmann <danielweinmann@gmail.com>
2011-06-29 16:22:37 -0300

    Creating tests for the Site model

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -8,8 +8,43 @@ describe Site do
   end
   
   it "should have a name" do
-    o = Factory.build(:category, :name => nil)
+    o = Factory.build(:site, :name => nil)
     o.should_not be_valid
+  end
+  
+  it "should have a host" do
+    o = Factory.build(:site, :host => nil)
+    o.should_not be_valid
+  end
+  
+  it "should have an unique name" do
+    Factory(:site, :name => "foo")
+    o = Factory.build(:site, :name => "foo")
+    o.should_not be_valid
+  end
+  
+  it "should have an unique host" do
+    Factory(:site, :host => "foo")
+    o = Factory.build(:site, :host => "foo")
+    o.should_not be_valid
+  end
+  
+  it "should have users" do
+    o = Factory(:site)
+    u = Factory(:user, :site => o)
+    o.users.should == [u]
+  end
+  
+  it "should have a global auth_gateway" do
+    gateway = Factory(:site, :auth_gateway => true)
+    o = Factory(:site)
+    Site.auth_gateway.should == gateway
+  end
+  
+  it "should have a full url, with optional path" do
+    o = Factory(:site, :host => "foo.com", :port => "8080")
+    o.full_url.should == "http://foo.com:8080"
+    o.full_url("/my_path?my_param=foobar").should == "http://foo.com:8080/my_path?my_param=foobar"
   end
   
 end
