c1d31967c91ecb49722a1b08466f9903806121be
Paulo Geyer <paulogeyer@gmail.com>
2011-06-29 22:51:10 -0300

    changing admin site title to Ramify.me

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = 'current_site.name'.to_sym
+  config.site_title = 'Ramify.me'
 
 
   # == Default Namespace
