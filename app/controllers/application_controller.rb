8cd67bb6f10ae04850b5bb344306a25a185fad7d
Daniel Weinmann <danielweinmann@gmail.com>
2011-06-30 00:52:45 -0300

    Making detect_locale always redirect to pt for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,12 +19,15 @@ class ApplicationController < ActionController::Base
     return if params[:locale]
     new_locale = current_user.locale if current_user
     new_locale = session[:locale] if session[:locale]
-    unless new_locale
-      new_locale = request.compatible_language_from(I18n.available_locales.map(&:to_s))
-      new_locale = I18n.default_locale.to_s unless new_locale
-      flash[:locale] = t('notify_locale', :locale => new_locale)
-    end
-    return redirect_to replace_locale(new_locale)
+    #unless new_locale
+    #  new_locale = request.compatible_language_from(I18n.available_locales.map(&:to_s))
+    #  new_locale = I18n.default_locale.to_s unless new_locale
+    #  flash[:locale] = t('notify_locale', :locale => new_locale)
+    #end
+    #return redirect_to replace_locale(new_locale)
+    
+    # TODO change back to original settings when we have multiple locales
+    return redirect_to replace_locale('pt')
   end
   
   def replace_locale(new_locale)
