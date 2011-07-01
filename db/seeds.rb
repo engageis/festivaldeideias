b2c948861bc929bb3c91a8365647c30e54d4d3c1
Paulo Geyer <paulogeyer@gmail.com>
2011-07-01 11:16:43 -0300

    fixing badge file url

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,10 +21,10 @@ site.links.create :name => "Vídeos", :href => '#'
 site.links.create :name => "Contato", :href => '#'
 
 # TODO create a badge for each category with carrier wave
-badge_path = "#{Rails.root.to_s}/lib/fixtures/badge.gif"
-category_1 = Category.create! :site => site, :name => "Mobilidade urbana", :badge => badge_path
-category_2 = Category.create! :site => site, :name => "Segurança comunitária", :badge => badge_path
-category_3 = Category.create! :site => site, :name => "Catástrofes naturais", :badge => badge_path
+badge_file = File.open("#{Rails.root.to_s}/lib/fixtures/badge.gif")
+category_1 = Category.create! :site => site, :name => "Mobilidade urbana", :badge => badge_file
+category_2 = Category.create! :site => site, :name => "Segurança comunitária", :badge => badge_file
+category_3 = Category.create! :site => site, :name => "Catástrofes naturais", :badge => badge_file
 
 user = User.create! :site => site, :provider => 'fake', :uid => 'foo_bar', :name => "Foo Bar"
 user_2 = User.create! :site => site, :provider => 'fake', :uid => 'bar_foo', :name => "Bar Foo"
