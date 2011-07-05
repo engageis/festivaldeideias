7e592d428f3bdbded190b07ec9e8eff5f64a13c9
Paulo Geyer <paulogeyer@gmail.com>
2011-07-05 15:31:47 -0300

    backbone model for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   inherit_resources
   actions :show
+  def show
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render :json => @user }
+    end
+  end
 end
