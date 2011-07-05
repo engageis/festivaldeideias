dd4a12f5981c3dcfa119e81d067fe3bfd5f5de97
Paulo Geyer <paulogeyer@gmail.com>
2011-07-05 17:48:22 -0300

    setting slim to output formatted html in the development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ Ramify::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  # set slim to output formatted html
+  Slim::Engine.set_default_options :pretty => true
 end
 
