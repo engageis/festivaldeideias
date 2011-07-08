41db885681e175749776af9ab7dccfb41713da09
Daniel Weinmann <danielweinmann@gmail.com>
2011-07-08 17:49:09 -0300

    Compass settings to work with Heroku

--- config/compass.rb
+++ config/compass.rb
@@ -5,6 +5,9 @@ project_type = :rails
 # Set this to the root of your project when deployed:
 http_path = "/"
 
+css_dir = "tmp/stylesheets"
+sass_dir = "app/stylesheets"
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
---
+++ config/initializers/compass.rb
@@ -0,0 +1,6 @@
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => "#{Rails.root}/tmp")
