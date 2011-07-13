b54fde2a4834e04e7a71ad52c37267d7a25f8c2b
Paulo Geyer <paulogeyer@gmail.com>
2011-07-13 00:36:46 -0300

    still trying to fix things with heroku

--- app/uploaders/badge_uploader.rb
+++ app/uploaders/badge_uploader.rb
@@ -7,11 +7,11 @@ class BadgeUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :s3
-  else
+  # if Rails.env.production?
+  #   storage :s3
+  # else
     storage :file
-  end
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
