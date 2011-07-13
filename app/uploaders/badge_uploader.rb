07c202ce346e91c93ced36fc2b6922cecd460ddf
Paulo Geyer <paulogeyer@gmail.com>
2011-07-13 02:21:45 -0300

    still trying to fix things with heroku

--- app/uploaders/badge_uploader.rb
+++ app/uploaders/badge_uploader.rb
@@ -16,6 +16,7 @@ class BadgeUploader < CarrierWave::Uploader::Base
   def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   # def store_dir
