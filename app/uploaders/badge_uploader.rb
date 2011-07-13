3f021c0b8a0ae888439de3a352a8c80d46d0df8f
Paulo Geyer <paulogeyer@gmail.com>
2011-07-12 22:11:18 -0300

    fixing problems to make app run on heroku round three

--- app/uploaders/badge_uploader.rb
+++ app/uploaders/badge_uploader.rb
@@ -7,17 +7,20 @@ class BadgeUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env == "production"
+  if Rails.env.production?
     storage :fog
   else
     storage :file
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
