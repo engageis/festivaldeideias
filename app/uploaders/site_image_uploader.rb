242becc04334a0f93419821350ec93995d8dddcd
Paulo Geyer <paulogeyer@gmail.com>
2011-07-13 00:33:44 -0300

    still trying to fix things with heroku

--- app/uploaders/site_image_uploader.rb
+++ app/uploaders/site_image_uploader.rb
@@ -10,9 +10,9 @@ class SiteImageUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
