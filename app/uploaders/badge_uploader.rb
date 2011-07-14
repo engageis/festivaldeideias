aac416b87200b0d5ed75f731d0e9fd6aa1773a98
Paulo Geyer <paulogeyer@gmail.com>
2011-07-14 18:37:37 -0300

    carrierwave/fog is working with aws s3 now. FINALLY

--- app/uploaders/badge_uploader.rb
+++ app/uploaders/badge_uploader.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 
-require 'mime/types'
-
 class BadgeUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or ImageScience support:
@@ -21,9 +19,9 @@ class BadgeUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
--- app/uploaders/site_image_uploader.rb
+++ app/uploaders/site_image_uploader.rb
@@ -10,9 +10,9 @@ class SiteImageUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   def cache_dir
     "#{Rails.root}/tmp/uploads"
