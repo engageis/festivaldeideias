85b87e73064e9dd8c9ad31e4bd35360931af02f4
Paulo Geyer <paulogeyer@gmail.com>
2011-07-15 15:33:55 -0300

    adjusting carrierwave to enable rake db:migrate

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,16 +4,19 @@ if Rails.env.test? or Rails.env.cucumber?
     config.enable_processing = false
   end
 elsif Rails.env.production?
-  CarrierWave.configure do |config|
-    access_key = Configuration.find_by_name('aws_access_key')
-    secret_key = Configuration.find_by_name('aws_secret_key')
-#    fog_directory = Configuration.find_by_name('fog_directory')
-    bucket = Configuration.find_by_name('aws_bucket')
+  begin
+    CarrierWave.configure do |config|
+      access_key = Configuration.find_by_name('aws_access_key')
+      secret_key = Configuration.find_by_name('aws_secret_key')
+  #    fog_directory = Configuration.find_by_name('fog_directory')
+      bucket = Configuration.find_by_name('aws_bucket')
 
-    if access_key and secret_key and bucket
-      config.s3_access_key_id = access_key.value
-      config.s3_secret_access_key = secret_key.value
-      config.s3_bucket = bucket.value
+      if access_key and secret_key and bucket
+        config.s3_access_key_id = access_key.value
+        config.s3_secret_access_key = secret_key.value
+        config.s3_bucket = bucket.value
+      end
     end
+  rescue
   end
 end
