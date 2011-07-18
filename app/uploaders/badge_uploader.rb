85186927820aade80ee9d9f1c1e8448d734fe5eb
Paulo Geyer <paulogeyer@gmail.com>
2011-07-18 06:10:46 -0300

    changing the store directory for badges

--- app/uploaders/badge_uploader.rb
+++ app/uploaders/badge_uploader.rb
@@ -13,6 +13,10 @@ class BadgeUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
   def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
