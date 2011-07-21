8d0ae884820ee6db4895092e7b83e13988d61e7e
Paulo Geyer <paulogeyer@gmail.com>
2011-07-21 20:14:04 -0300

    customising the site admin form

--- app/admin/sites.rb
+++ app/admin/sites.rb
@@ -5,4 +5,21 @@ ActiveAdmin.register Site do
     controller.current_ability.can?(:manage, Site)
   }
 
+  index do
+    column :name do |site|
+      link_to site.name, admin_site_path(site)
+    end
+  end
+
+  form :html => {:multipart => true} do |f|
+    f.inputs do
+      f.input :name, :as => :string
+      f.input :host, :as => :string
+      f.input :port, :as => :string
+      f.input :auth_gateway
+      f.input :twitter, :as => :string
+      f.input :image, :as => :file
+    end
+  end
+
 end
