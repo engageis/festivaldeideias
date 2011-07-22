4570e860b7eea2288d5422d29b4b6f9757bfb718
Daniel Weinmann <danielweinmann@gmail.com>
2011-07-22 18:42:09 -0300

    Putting back the Conflict exception filter in order to get an error when a merge returns 404

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -94,7 +94,7 @@ class Idea < ActiveRecord::Base
       self.save
       self.document = merged_document
       merge.finished = true
-    rescue
+    rescue RestClient::Conflict
       merge.pending = true
     end
     self.merging = false
