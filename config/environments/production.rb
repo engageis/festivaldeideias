bc879eb382fa3d01041f4fca1fb830bc7955844d
Daniel Weinmann <danielweinmann@gmail.com>
2011-08-10 16:57:13 -0300

    Making Heroku log SQLs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Ramify::Application.configure do
+
+  # Making Heroku log SQLs
+  config.logger = Logger.new(STDOUT)
+  
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
