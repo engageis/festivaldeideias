42430587e1dac856de087da9d4a2dd0d47a23f8f
Natalia Arsand <nataliarsand@gmail.com>
2011-12-26 14:12:37 -0200

    added idea page and basic structure/styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < InheritedResources::Base
   def most_active_ideas
 
   end
+
+  def ideas
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ FestivalDeIdeias::Application.routes.draw do
   match '/ideas/mais-colaboradas' => "pages#most_active_ideas", :as => :most_active_ideas
   resources :users
   get "/miv" => "miv#index" if Rails.env.development?
+  get "/idea" => "pages#idea"
 
   root :to => "pages#most_active_ideas"
 
