945b8ee74c213550aa6b52b2374c6d847e5031a4
Luiz Fonseca <runeroniek@gmail.com>
2011-12-28 11:57:32 -0300

    Spec/controller for Session and Migration for ideas. The fun has just began.

---
+++ db/migrate/20111228050148_create_ideas.rb
@@ -0,0 +1,22 @@
+class CreateIdeas < ActiveRecord::Migration
+  def up
+    create_table :ideas do |t|
+      t.integer :user_id, :null => false
+      t.integer :parent_id, :null => false
+      t.text    :title, :null => false
+      t.text    :headline, :null => false
+      t.text    :description, :null => false
+      t.boolean :featured, :null => false, :default => false
+      t.boolean :recommend, :null => false, :default => false
+      t.integer :likes, :null => false, :default => 0
+      t.integer :position
+      t.timestamps
+    end
+    add_foreign_key :ideas, :users
+    add_foreign_key :ideas, :ideas, :column => :parent_id
+  end
+
+  def down
+    drop_table :ideas
+  end
+end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe SessionsController do
-
+  describe "POST #create" do
+    before do
+      @auth = FACEBOOK_HASH_DATA
+      request.env['omniauth.auth'] = @auth
+      post :create, :provider => :facebook
+    end
+    context "When the user is not yet in the service list" do
+      before do
+        Service.stub(:find_from_hash).and_return(false)
+        @hash = Service.stub(:create_from_hash).and_return("current_user")
+      end
+      it { should redirect_to root_path }
+    end
+    context "When the user is in the service list" do
+      before do
+        @auth = Factory.create(:service, :uid => FACEBOOK_HASH_DATA["uid"])
+        Service.stub(:find_from_hash).and_return(true)
+      end
+      it { should redirect_to root_path }
+    end
+  end
 end
