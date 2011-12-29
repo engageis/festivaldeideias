9cfc9aeeae12d4a6553564fd4892236f612d16f7
Luiz Fonseca <runeroniek@gmail.com>
2011-12-29 15:33:05 -0300

    Added cucumber steps, and implementation of the idea index list. Needing CSS improvements tho. Also factory for idea.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -2,6 +2,8 @@
 Factory.sequence(:name)   { |n| "User #{n}" }
 Factory.sequence(:email)  { |n| "user#{n}@gmail.com" }
 Factory.sequence(:uid)    { |n| "#{n}" }
+Factory.sequence(:title)  { |n| "Title#{n}" }
+Factory.sequence(:headline) { |n| "Headline#{n}" }
 
 # Defining user factory
 Factory.define :user do |u|
@@ -14,4 +16,11 @@ Factory.define :service do |s|
   s.association :user, :factory => :user
   s.uid(Factory.next(:uid))
   s.provider("facebook")
+end
+
+Factory.define :idea do |i|
+  i.association :user, :factory => :user
+  i.title(Factory.next(:title))
+  i.headline(Factory.next(:headline))
+  i.description("Some description")
 end
