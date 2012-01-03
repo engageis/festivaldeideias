984dc06c210dbbf2fe0f0834006dbbd4dc2b5b39
Luiz Fonseca <runeroniek@gmail.com>
2012-01-03 09:36:52 -0300

    Added support for i18n.

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,12 @@ module FestivalDeIdeias
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = "pt-BR"
+    config.i18n.locale = "pt-BR"
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
