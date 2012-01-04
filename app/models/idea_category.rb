3b9bb0ff2f1f2e2b74eaad05221bb1948799ddfb
Luiz Fonseca <runeroniek@gmail.com>
2012-01-04 18:21:44 -0300

    Just like ideas, making the to_param friendly to the user

--- app/models/idea_category.rb
+++ app/models/idea_category.rb
@@ -1,6 +1,11 @@
 class IdeaCategory < ActiveRecord::Base
-  has_many :ideas
+  has_many :ideas, :dependent => :destroy
   validates_presence_of :name, :badge
 
   mount_uploader :badge, BadgeUploader
+
+
+  def to_param
+    "#{self.id}-#{self.name.parameterize}"
+  end
 end
