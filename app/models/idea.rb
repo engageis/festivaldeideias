c7d1e0bc4614ee3835096b90fcc55e92a70da171
Luiz Fonseca <runeroniek@gmail.com>
2012-01-04 18:22:07 -0300

    Validating presence of category

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,6 @@
 class Idea < ActiveRecord::Base
 
-  validates_presence_of :title, :headline
+  validates_presence_of :title, :headline, :category
   belongs_to :user
   belongs_to :category, :class_name => "IdeaCategory", :foreign_key => :idea_category_id
   belongs_to :parent, :class_name => :Idea, :foreign_key => :parent_id
