1c2caa9564c3409d03a79bb2490fcd1b57a88609
Carlos Francisco Pinheiro <carlosfpinheiro@gmail.com>
2012-01-12 18:14:28 -0200

    Css modificado pros links das abas do form de idea ficarem melhor, agora só vai pra frente se houver descricao, categoria e titulo

--- app/assets/javascripts/backbone/views/ideas/newidea.js
+++ app/assets/javascripts/backbone/views/ideas/newidea.js
@@ -4,11 +4,11 @@ App.Ideas.NewIdea = App.BaseView.extend({
                 ,"showDescription"
                 ,"showRefinement"
                 ,"showPublishing"
-                ,"updateActiveLink"
                 ,"updatePublishingFields"
                 ,"selectCategory"
                 ,"updateStore"
                 ,"openIdeaForm"
+                ,"updateCharactersLeft"
             );
         var me = this;
         //$('.popup form').on('load', function () {
@@ -23,11 +23,12 @@ App.Ideas.NewIdea = App.BaseView.extend({
         "click a[href='#describe']": 'showDescription',
         "click a[href='#refine']": 'showRefinement',
         "click a[href='#publish']": 'showPublishing',
-        "click .popup a": "updateActiveLink",
         "click .popup #refine .categories li": "selectCategory",
         "blur .popup input": "updateStore",
         "blur .popup textarea": "updateStore",
-        "click a.start[href=#start]": "loadIdeaFromStore"
+        "click a.start[href=#start]": "loadIdeaFromStore",
+        "keydown .popup #idea_headline": "updateCharactersLeft",
+        "keyup .popup #idea_headline": "updateCharactersLeft",
     },
 
     showDescription: function () {
@@ -35,6 +36,7 @@ App.Ideas.NewIdea = App.BaseView.extend({
         box.find("#refine").addClass('hidden');
         box.find("#publish").addClass('hidden');
         box.find("#describe").removeClass('hidden');
+        this.updateActiveLink('describe');
     },
 
     showRefinement: function () {
@@ -44,34 +46,39 @@ App.Ideas.NewIdea = App.BaseView.extend({
         box.find("#describe").addClass('hidden');
         box.find("#publish").addClass('hidden');
         box.find("#refine").removeClass('hidden');
+        this.updateActiveLink('refine');
+        this.updateCharactersLeft();
     },
 
     showPublishing: function () {
         var box;
-        if (!this.hasDescription()) return false;
+        if (!this.hasDescription() || !this.hasCategory() || !this.hasTitle()) return false;
         box = $('.popup');
         box.find("#describe").addClass('hidden');
         box.find("#refine").addClass('hidden');
         box.find("#publish").removeClass('hidden');
+        this.updateActiveLink('publish');
         this.updatePublishingFields();
     },
 
-    updateActiveLink: function (e) {
-        var shortcuts, hash;
-        if (!this.hasDescription()) return false;
-        shortcuts = $('.popup .short_cuts a');
-        hash = e.target.hash;
-        shortcuts.each(function () {
-            if (this.hash === hash) {
-                $(this).parent().addClass('active');
-            } else {
-                $(this).parent().removeClass('active');
-            }
-        });
+    updateActiveLink: function (link) {
+        var lis = $('.popup .short_cuts li');
+        lis.removeClass('active');
+        lis.filter(function () {
+            return $(this).find('a[href=#'+link+']').size() > 0;
+        }).addClass('active');
     },
 
     hasDescription: function () {
         return jQuery.trim($('.popup').find('#idea_description').val()).length > 0;
+    },
+
+    hasCategory: function () {
+        return $('.popup').find('.categories :radio:checked').size() > 0;
+    },
+
+    hasTitle: function () {
+        return $('.popup').find('#idea_title').val().length > 0;
     },
 
     selectCategory: function (e) {
@@ -84,6 +91,13 @@ App.Ideas.NewIdea = App.BaseView.extend({
         }
         clickedListItem.find(':radio').prop('checked', true);
         this.updateStore();
+    },
+
+    updateCharactersLeft: function (e) {
+        var t, c;
+        t = this.headline || $('.popup #idea_headline');
+        c = this.counter  || $('.popup .char_counter');
+        c.text(200 - t.val().length);
     },
 
     updatePublishingFields: function () {
