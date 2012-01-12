aeb692e26d6b09a5f057772cde8680845837cc13
Luiz Fonseca <runeroniek@gmail.com>
2012-01-12 16:07:26 -0300

    Set success message for create and paginate

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,19 +1,17 @@
 class Idea < ActiveRecord::Base
-  attr_accessible :description, :title, :headline, :category, :category_id
 
+  validates_presence_of :title, :headline, :category
   belongs_to :user
   belongs_to :category, :class_name => "IdeaCategory", :foreign_key => :category_id
   belongs_to :parent, :class_name => :Idea, :foreign_key => :parent_id
 
-  validates_presence_of :title, :description, :category, :user
-
   scope :featured,  where(:featured => true).order('position DESC')
   scope :latest,    order('updated_at DESC')
   scope :recent,    order('created_at DESC')
+  scope :popular,   order('likes DESC')
 
 
   def to_param
     "#{id}-#{title.parameterize}"
   end
-
 end
