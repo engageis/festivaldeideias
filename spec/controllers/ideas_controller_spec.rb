3ba38baf95ad0dbd385692acb65470ee5890f782
Luiz Fonseca <runeroniek@gmail.com>
2012-01-12 13:10:04 -0300

    Specs for create action on IdeasController

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -9,4 +9,21 @@ describe IdeasController do
     its(:status) { should == 200 }
   end
 
+
+  describe "POST #create" do
+    before do
+      request.env['HTTP_REFERER'] = root_url
+      @category = Factory.create(:idea_category)
+      @user = Factory.create(:service).user
+      idea = { :title => "Just a test dude", :headline => "Headline test", :description => "Hey, I'm a test", }
+
+      controller.stub(:current_member).and_return(@user)
+
+      post :create, :category_id => @category.id, :idea => idea
+    end
+
+    its(:status) { should == 302 }
+    it { should redirect_to :back }
+  end
+
 end
