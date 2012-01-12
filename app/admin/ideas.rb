d114231bd8d7cb74d28893a42b7bc8c3f582a4d4
Luiz Fonseca <runeroniek@gmail.com>
2012-01-12 18:40:41 -0300

    Process ideas for exibihition on homepage + cucumber test

--- app/admin/ideas.rb
+++ app/admin/ideas.rb
@@ -1,4 +1,19 @@
 # coding: utf-8
 ActiveAdmin.register Idea do
+  menu :label => "Curação de ideias"
+
+  index do
+    column "Capa?" do |s|
+      span s.featured?
+    end
+    column "Posição" do |s|
+      span s.position
+    end
+    column :title
+    column :headline
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 end
 
