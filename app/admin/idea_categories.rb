eb4e8613eed873e5b2c2b017843d528165156a2d
Luiz Fonseca <runeroniek@gmail.com>
2012-01-13 03:24:02 -0300

    Change how the user see the categories list on admin interface

--- app/admin/idea_categories.rb
+++ app/admin/idea_categories.rb
@@ -2,13 +2,23 @@ ActiveAdmin.register IdeaCategory do
   menu :label => "Categorias"
 
 
+  index do
+    column :badge do |s|
+      image_tag s.badge.url, :size => "31x31"
+    end
+    column :id
+    column :name
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 
   form do |f|
     f.inputs "Details" do
       f.input :name, :as => :string
       f.input :description, :as => :string
     end
-    f.inputs "Files" do
+    f.inputs "Badge" do
       f.input :badge, :as => :file
     end
     f.buttons
