3c05a004ac0f6ce5374402e58549d3df7215e26c
Luiz Fonseca <runeroniek@gmail.com>
2012-01-13 03:28:01 -0300

    Put some order in route config.

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,22 @@ FestivalDeIdeias::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  match '/auth/:provider/callback', :to => 'sessions#create', :as => :session_create
-  match '/logout', :to => 'sessions#destroy', :as => :session_destroy
+  match '/auth/:provider/callback', :to => 'sessions#create',   :as => :session_create
+  match '/logout',                  :to => 'sessions#destroy',  :as => :session_destroy
 
 
   resources :users
   resources :ideas, :only => [:create]
 
-  get "sobre-o-festival" => "pages#about"
-  get "sobre-os-temas" => "pages#themes"
-  get "regulamento" => "pages#regulation"
-  get "premiacao" => "pages#awards"
-  get "navegue-nas-ideias" => "ideas#navigate"
+  get "sobre-o-festival"    => "pages#about",       :as => :about_page
+  get "sobre-os-temas"      => "pages#themes",      :as => :themes_page
+  get "regulamento"         => "pages#regulation",  :as => :regulation_page
+  get "premiacao"           => "pages#awards",      :as => :awards_page
+  get "navegue-nas-ideias"  => "ideas#navigate",    :as => :navigate_page
 
-  match ":category_id/ideia/:id", :to => "ideas#show", :as => :category_idea
-  match ":category_id/ideias", :to => "ideas#index", :as => :category_ideas
+  match ":idea_category_id/ideia/:id",   :to => "ideas#show",  :as => :category_idea
+  match ":idea_category_id/ideias",      :to => "ideas#index", :as => :category_ideas
 
   get "/miv" => "miv#index" if Rails.env.development?
   root :to => "ideas#index"
-
 end
