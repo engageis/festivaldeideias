59c5ebc6772389b9de49c831b5e8ea0428c7ef72
Carlos Francisco Pinheiro <carlosfpinheiro@gmail.com>
2012-01-13 14:29:50 -0200

    Transferindo botão de começar ideia para um helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def new_idea_button(text = nil)
+    text ||= t('buttons.new_idea')
+    if current_user
+      link_to text, '#start', :rel => 'facebox', :class => 'button start'
+    else
+      link_to text, '#login', :rel => 'facebox', :class => 'button start', :'data-return-url' => '#continue_idea' 
+    end
+  end
+
 end
