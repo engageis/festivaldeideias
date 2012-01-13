c5144e80579c9d0ab8de39e821873a0596b00b59
Luiz Fonseca <runeroniek@gmail.com>
2012-01-13 03:30:00 -0300

    Remove default scope option and unnecessary code.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,28 +2,32 @@ class IdeasController < ApplicationController
 
   inherit_resources
 
-  has_scope :featured, :type => :boolean, :default => false, :only => :index
+  has_scope :featured, :type => :boolean, :only => :index
+  has_scope :popular
   has_scope :latest
   has_scope :recent
 
   belongs_to :idea_category, :optional => true
 
-  respond_to :html, :json
+  respond_to :html, :except => [:update]
+  respond_to :json, :only => [:index, :update]
+
   before_filter :load_resources
 
   def create
     @idea = Idea.new(params[:idea])
     @idea.user = current_user if current_user
-    create!(:notice => t('idea.message.success'),
-            :warning => t('idea.message.failure')) { request.referer }
+    create!(
+            :notice => t('idea.message.success'),
+            :alert => t('idea.message.failure')
+    ) { request.referer }
   end
 
   protected
   def load_resources
-    @ideas ||= end_of_association_chain.page params[:page]
     @categories ||= parent? ? parent.idea_categories : IdeaCategory.all
     @users ||= User.all(:include => :services)
-    @ideas_count ||= @ideas.count
+    @ideas_count ||= Idea.count
     @ideas_latest ||= Idea.latest
     @ideas_featured ||= Idea.featured
   end
