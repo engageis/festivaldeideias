89ffa913bbbdec413494eb9aabe2d6916ce87e99
Luiz Fonseca <runeroniek@gmail.com>
2012-01-16 16:10:11 -0300

    Bind editable fields with mousedown

--- app/assets/javascripts/backbone/views/ideas/show.js
+++ app/assets/javascripts/backbone/views/ideas/show.js
@@ -1,8 +1,14 @@
 App.Ideas.Show = App.BaseView.extend({
 
   initialize: function(){
+    this.model = new App.Models.Idea
     _.bindAll(this)
+    this.model.bind('save', this.model.save);
+  },
 
-  }
+  events: {
+    'mousedown .editable' : 'editableClick'
+  },
 
+  editableClick: etch.editableInit
 });
