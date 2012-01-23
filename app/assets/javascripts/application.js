7cee561272b59c4d16148f975d83ae8e6b0a882b
Carlos Francisco Pinheiro <carlosfpinheiro@gmail.com>
2012-01-23 03:19:26 -0200

    Incluindo etch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,6 +8,7 @@
 //= require backbone_rails_sync
 //= require backbone_datalink
 //= require libraries/rangy
+//= require libraries/etch
 //= require libraries/editable
 //= require backbone/app
 //= require init
--- public/javascripts/Player.js
+++ public/javascripts/Player.js
@@ -1,22 +1,52 @@
 function Player() {
+    this.currentlyPlayingSong = null;
+    this.isPlaying = false;
 }
-Player.prototype.play = function(song) {
-  this.currentlyPlayingSong = song;
-  this.isPlaying = true;
+
+Player.prototype = {
+    constructor: Player,
+
+    play: function (song) {
+        this.currentlyPlayingSong = song;
+        this.resume();
+    },
+
+    pause: function () {
+        this.isPlaying = false;
+    },
+
+    resume: function () {
+        if (this.isPlaying) {
+            throw new Error("song is already playing");
+        } else if (this.currentlyPlayingSong === null) {
+            throw new Error("no song to played");
+        } else {
+            this.isPlaying = true;
+        }
+    },
+
+    makeFavourite: function () {
+        this.currentlyPlayingSong.persistFavoriteStatus(true);
+    }
 };
 
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
+//Player.prototype.play = function(song) {
+  //this.currentlyPlayingSong = song;
+  //this.isPlaying = true;
+//};
 
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
-  }
+//Player.prototype.pause = function() {
+  //this.isPlaying = false;
+//};
 
-  this.isPlaying = true;
-};
+//Player.prototype.resume = function() {
+  //if (this.isPlaying) {
+    //throw new Error("song is already playing");
+  //}
 
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
-};
+  //this.isPlaying = true;
+//};
+
+//Player.prototype.makeFavorite = function() {
+  //this.currentlyPlayingSong.persistFavoriteStatus(true);
+//};
