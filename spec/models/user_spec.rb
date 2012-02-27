be7ece2bd404bd206b7bda7eb48ecfd5cd4b4bf8
Luiz Fonseca <runeroniek@gmail.com>
2012-02-27 11:23:59 -0300

    Insert collaborations and ideas count to the user actions menu

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ describe User do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should have_many :ideas }
+    it { should have_many :colaborations }
 
     describe "#services" do
       it { should have_many :services }
