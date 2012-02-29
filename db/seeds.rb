1dbcd1acf7f414d18018bd0fd07689f64a531f04
Luiz Fonseca <runeroniek@gmail.com>
2012-02-29 03:33:10 -0300

    Comment out ideas from seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-# 
+#
 # Examples:
-# 
+#
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
@@ -18,6 +18,7 @@ c3 = IdeaCategory.create(:name => "Violência", :description => "Sample descript
 
 
 # Ideas
+=begin
 Idea.create!([
   { :category => c1, :user => user, :title => "Carona solidária", :headline => "Ao sair de casa informa seu percurso no sistema, seus contados confiáveis recebem a informação e podem se cadastrar para pegar uma carona", :description => "test?" },
   { :category => c2, :user => user, :title => "Carona solidária", :headline => "Ao sair de casa informa seu percurso no sistema, seus contados confiáveis recebem a informação e podem se cadastrar para pegar uma carona", :description => "test?" },
@@ -27,7 +28,7 @@ Idea.create!([
   { :category => c1, :user => user, :title => "Carona solidária", :headline => "Ao sair de casa informa seu percurso no sistema, seus contados confiáveis recebem a informação e podem se cadastrar para pegar uma carona", :description => "test?" },
 
 ])
-
+=end
 %W(miguxo@facebooksucks.com juquinha@amoorkut.com sem.idea@email.com).each do |email|
   NonFacebookUser.create(:email => email)
 end
