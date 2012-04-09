25ef11aa23d17b715367ebdf12ce82d513cce674
Luiz Fonseca <runeroniek@gmail.com>
2012-04-09 12:05:09 -0300

    Remove carrierwave initializer - we aren't dealing with badge uploader anymore. Maybe later?

--- app/models/idea_category.rb
+++ app/models/idea_category.rb
@@ -2,7 +2,6 @@ class IdeaCategory < ActiveRecord::Base
 	has_many :ideas, :dependent => :destroy, :foreign_key => :category_id
 	validates_presence_of :name
 
-	#mount_uploader :badge, BadgeUploader
 
   def to_param
    "#{self.id}-#{self.name.parameterize}"
