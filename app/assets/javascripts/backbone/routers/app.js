568fb4c2133376b3be7365aed2243c4b569dcf14
Luiz Fonseca <runeroniek@gmail.com>
2012-04-11 23:45:28 -0300

    Remove crappy code.

--- app/assets/javascripts/backbone/routers/app.js
+++ app/assets/javascripts/backbone/routers/app.js
@@ -2,20 +2,7 @@ App.Router = Backbone.Router.extend({
 
     routes: {
         "colaborate" : "colaborate",
-        "continue_idea": "loadIdeaFromStore",
-        //"login": "loginAction", // Acaba acontencendo duas vezes, por causa do router e por causa do evento
-    },
-
-    // You can let these functions empty
-    // and bind the routes in the view.
-    // Nevertheless you still need to define them.
-    colaborate: function(){},
-
-    ideaBox: function () {
-    },
-
-    loginAction: function () {
-        $.facebox({ div: "#login" });
+        "continue_idea": "loadIdeaFromStore"
     },
 
     loadIdeaFromStore: function () {
