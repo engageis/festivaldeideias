7a7324149b46d0b3f91f883b2112b4689488c7cb
Luiz Fonseca <runeroniek@gmail.com>
2012-04-15 11:55:57 -0300

    Add dashboard to show colaborations and recent ideas.

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 ActiveAdmin::Dashboards.build do
 
   # Define your dashboard sections here. Each block will be
@@ -6,14 +7,23 @@ ActiveAdmin::Dashboards.build do
   
   # == Simple Dashboard Section
   # Here is an example of a simple dashboard section
-  #
-  #   section "Recent Posts" do
-  #     ul do
-  #       Post.recent(5).collect do |post|
-  #         li link_to(post.title, admin_post_path(post))
-  #       end
-  #     end
-  #   end
+  
+  section "Ideias Recentes" do
+   ul do
+     Idea.recent(5).collect do |idea|
+       li link_to(idea.title, admin_idea_path(idea))
+     end
+   end
+  end
+
+
+  section "Colaborações Recentes" do
+   ul do
+     Idea.where(['parent_id IS NOT NULL']).recent(5).collect do |idea|
+       li link_to(idea.title, admin_idea_path(idea))
+     end
+   end
+  end
   
   # == Render Partial Section
   # The block is rendered within the context of the view, so you can
