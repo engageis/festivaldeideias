dede9b4a8089106303a9af9cba084b2c5defd34e
Luiz Fonseca <runeroniek@gmail.com>
2012-04-18 14:53:11 -0300

    Cleanup.

--- config/initializers/01-load_config.rb
+++ config/initializers/01-load_config.rb
@@ -1,10 +1,9 @@
-CONFIG = []
 unless Rails.env.production?
   CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
   ENV['FB_APP_ID'] = CONFIG['FB_APP_ID']
   ENV['FB_APP_SECRET'] = CONFIG['FB_APP_SECRET']
   ENV['FB_CALLBACK_URL'] = CONFIG['FB_CALLBACK_URL']
 end
-require "koala"
-oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], ENV['FB_CALLBACK_URL'])
-CONFIG['facebook_access_token'] = oauth.get_app_access_token
+#require "koala"
+#oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], ENV['FB_CALLBACK_URL'])
+#CONFIG['facebook_access_token'] = oauth.get_app_access_token
