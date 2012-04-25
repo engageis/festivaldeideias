4cc595b64cba9ee769cb06484494d92b57be1b81
Luiz Fonseca <runeroniek@gmail.com>
2012-04-25 00:46:08 -0400

    Remove old factory girl.

--- spec/factories/notifications.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :notification do
-    user_id 1
-    message "MyText"
-    read false
-  end
-end
