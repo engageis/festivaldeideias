a9049af07ffc36cf4c3212410a4c48fbae067299
Luiz Fonseca <runeroniek@gmail.com>
2012-06-05 01:32:54 -0300

    Add test-only key & secret for tokbox.

--- config/initializers/01-load_config.rb
+++ config/initializers/01-load_config.rb
@@ -1,6 +1,16 @@
+require 'opentok'
+key, secret = "15793291", "b6f90ff600f642f98b00d044161cbee6996045ed"
+
+
 unless Rails.env.production?
   CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
   ENV['FB_APP_ID'] = CONFIG['FB_APP_ID']
   ENV['FB_APP_SECRET'] = CONFIG['FB_APP_SECRET']
   ENV['FB_CALLBACK_URL'] = CONFIG['FB_CALLBACK_URL']
+end
+
+if Rails.env.production?
+  TOKBOX = OpenTok::OpenTokSDK.new ENV['TOKBOX_KEY'], ENV['TOKBOX_SECRET'], :api_url => 'https://api.opentok.com/hl'
+else
+  TOKBOX = OpenTok::OpenTokSDK.new key, secret
 end
