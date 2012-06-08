9616cfaec9e44be116a694b9b4f3ed7ae138f2c5
Nícolas Iensen <nicolas@engage.is>
2012-06-08 10:27:39 -0300

    Fix idea#cocreate view

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20120605232831) do
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string   "remember_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
