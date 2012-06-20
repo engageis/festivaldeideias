a3b0a8124bf82c0ca2f957dac547c25a53129fcf
Luiz Fonseca <runeroniek@gmail.com>
2012-06-20 16:26:00 -0300

    Uses ENV vars in production and yaml CONFIG-based on development.

--- config/initializers/01-load_config.rb
+++ config/initializers/01-load_config.rb
@@ -1,16 +1,25 @@
 require 'opentok'
 require 'pusher'
-key, secret = "15793291", "b6f90ff600f642f98b00d044161cbee6996045ed"
-
+CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+ENV['TOKBOX_API_URL']     = CONFIG['TOKBOX_API_URL']
+ENV['TOKBOX_JS_URL']      = CONFIG['TOKBOX_JS_URL']
 
 unless Rails.env.production?
-  CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
-  ENV['FB_APP_ID'] = CONFIG['FB_APP_ID']
-  ENV['FB_APP_SECRET'] = CONFIG['FB_APP_SECRET']
-  ENV['FB_CALLBACK_URL'] = CONFIG['FB_CALLBACK_URL']
+  ENV['FB_APP_ID']        = CONFIG['FB_APP_ID']
+  ENV['FB_APP_SECRET']    = CONFIG['FB_APP_SECRET']
+  ENV['FB_CALLBACK_URL']  = CONFIG['FB_CALLBACK_URL']
+  ENV['TOKBOX_KEY']       = CONFIG['TOKBOX_KEY']
+  ENV['TOKBOX_SECRET']    = CONFIG['TOKBOX_SECRET']
+  ENV['PUSHER_KEY']       = CONFIG['PUSHER_KEY']
+  ENV['PUSHER_SECRET']    = CONFIG['PUSHER_SECRET']
 end
 
-TOKBOX = OpenTok::OpenTokSDK.new key, secret
-Pusher.app_id = '22022'
-Pusher.key    = '3e107fc25d5330b3338b'
-Pusher.secret = '2b7c5990aafe688f3080'
+TOKBOX = OpenTok::OpenTokSDK.new(
+  ENV['TOKBOX_KEY'], 
+  ENV['TOKBOX_SECRET'], 
+  :api_url => ENV['TOKBOX_API_URL']
+)
+
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key    = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
