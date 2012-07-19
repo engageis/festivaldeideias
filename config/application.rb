ed05250fd7435f2584466ee5f762f1541efa7137
Daniel Weinmann <danielweinmann@gmail.com>
2012-07-19 19:42:12 -0300

    Disabling the .field_with_errors div, so it don't mess with our CSS

--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,10 @@ module FestivalDeIdeias
         config.preferred_syntax = :sass
       end
     end
+    
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      "#{html_tag}".html_safe 
+    }
+    
  end
 end
